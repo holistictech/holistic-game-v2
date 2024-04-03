b1e30eb9837590ce3b2b8d189fefb6a6ece59d7f
Ahmet Hakan Hafif <59022934+colegion@users.noreply.github.com>
2024-04-03 03:16:04 +0300

    feat
    forward span scenario 1.6.1 base class

---
+++ Assets/Scripts/Spans/ForwardSpanBelongingsDescription.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Spans.Skeleton;
+using UnityEngine;
+
+namespace Spans
+{
+    public class ForwardSpanBelongingsDescription : SpanController
+    {
+        [SerializeField] private AudioClip[] clips;
+        public override List<object> GetSpanObjects()
+        {
+            return GetRandomClips();
+        }
+        
+        public override int GetRoundTime()
+        {
+            return currentRoundIndex * 3 + 2;
+        }
+
+        private List<object> GetRandomClips()
+        {
+            List<object> shuffledSprites = new List<object>(clips);
+            for (int i = 0; i < shuffledSprites.Count; i++)
+            {
+                int randomIndex = Random.Range(i, shuffledSprites.Count);
+                (shuffledSprites[i], shuffledSprites[randomIndex]) = (shuffledSprites[randomIndex], shuffledSprites[i]);
+            }
+            
+            List<object> selected = new List<object>();
+            for (int i = 0; i < currentRoundIndex; i++)
+            {
+                selected.Add(shuffledSprites[i]);
+            }
+
+            currentSpanQuestions = selected;
+            return selected;
+        }
+    }
+}
