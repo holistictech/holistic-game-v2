cdc4dc4c6fc02f7050be8ae66cb95cdd181ed31f
Ahmet Hakan Hafif <59022934+colegion@users.noreply.github.com>
2024-05-15 17:54:17 +0300

    WM/feat/86byrx4zu
    a mesh container since meshes are not serializable to save as JSON in unity

---
+++ Assets/Scripts/Utilities/MeshContainer.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utilities
+{
+    public class MeshContainer : MonoBehaviour
+    {
+        [SerializeField] private List<Mesh> meshList;
+
+        private static Dictionary<int, Mesh> MeshDictionary = new Dictionary<int, Mesh>();
+
+        private static MeshContainer _instance;
+        
+        public static MeshContainer Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<MeshContainer>();
+
+                    if (_instance == null)
+                    {
+                        GameObject singletonObject = new GameObject("MeshContainer");
+                        _instance = singletonObject.AddComponent<MeshContainer>();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        public static Mesh GetMeshById(int id)
+        {
+            if (MeshDictionary.ContainsKey(id))
+            {
+                MeshDictionary.TryGetValue(id, out Mesh mesh);
+                return mesh;
+            }
+            else
+            {
+                throw new Exception("ID could not be found :" + id);
+                return new Mesh();
+            }
+        }
+    }
+}
