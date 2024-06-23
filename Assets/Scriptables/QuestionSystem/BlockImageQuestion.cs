f0aeac9c1865bd9943845ee25292f12543fff8a5
Ahmet Hakan Hafif <59022934+colegion@users.noreply.github.com>
2024-06-23 16:35:21 +0300

    HP/asset/86bz9xzzd/86bza2bea
    created new assets appropriate with the requirements.

---
+++ Assets/Scriptables/QuestionSystem/BlockImageQuestion.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+using Utilities.Helpers;
+
+namespace Scriptables.QuestionSystem
+{
+    [CreateAssetMenu(fileName = "Question", menuName = "Question/BlockImage")]
+    public class BlockImageQuestion : Question
+    {
+        public Sprite Sprite;
+        public int Index;
+        private Sprite _initialSprite;
+        
+        public override void SetQuestionItem(object item)
+        {
+            Sprite = (Sprite)item;
+        }
+
+        public override object GetQuestionItem()
+        {
+            return Sprite;
+        }
+        
+        private bool _isInitial = true;
+
+        public override void ResetSelf()
+        {
+            if (Sprite != _initialSprite && !_isInitial)
+            {
+                Sprite = _initialSprite;
+                _isInitial = false;
+            }
+        }
+        
+        public override object GetQuestionItemByType(CommonFields.ButtonType type)
+        {
+            return Index;
+        }
+    }
+}
